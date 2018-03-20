0d1ac5a427a17e502c80abb9186fc73897b64479
Vu Cong <vungoccong@Congs-MacBook-Pro.local>
2018-03-20 17:17:07 +0700

    Initial Commit

---
+++ PassDataWithNotificationCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataWithNotificationCenter
+//
+//  Created by Vu Ngoc Cong on 3/20/18.
+//  Copyright © 2018 Vu Ngoc Cong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
